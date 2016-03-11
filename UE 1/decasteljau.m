0bc856428b4c944f6abbdcce48ecde025b70b77c
Stefan Zaufl <stefan.zaufl@hotmail.com>
2016-03-11 10:07:11 +0100

    init commit

---
+++ UE 1/decasteljau.m
@@ -0,0 +1,7 @@
+function [ output_args ] = decasteljau( input_args )
+%DECASTELJAU Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ UE 1/pureDecasteljau.m
@@ -0,0 +1,10 @@
+function [ c ] = pureDecasteljau( b, t )
+%PUREDECASTELJAU computes the point of the bézier curve using the algorithm of De Casteljau.
+%   b are the control points of the curve
+%   t is the parameter of the curve, must be between 0 and 1
+%   returns c, the point on the curve.
+
+
+
+end
+
