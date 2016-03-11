2a21317043ff3120f1c3f5db2eea5481d1e002ac
Stefan Zaufl <stefan.zaufl@hotmail.com>
2016-03-11 11:31:54 +0100

    First working version of the algoithm of de castelau.

---
+++ UE 1/main.m
@@ -0,0 +1,19 @@
+%Control points
+b = [1 3 5.5 6.5 5 4;2 4 4.5 2.5 1 2.5];
+%parameters to evaluate
+t = 0 : 0.01 : 1;
+%points on curve
+c = zeros(size(b, 1), size(t, 2));
+
+%Compute a curve point for each parameter
+for i = 1 : size(t, 2)
+    c(:, i) = pureDecasteljau(b, t(i));
+end
+
+%draw it!
+figure
+plot(b(1, :), b(2, :), '-r' )
+hold on;
+plot(c(1,:), c(2,:), '-b')
+hold off;
+title('Bézier Curve')
