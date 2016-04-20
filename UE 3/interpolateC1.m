bb7b7d7165815a8fefa3470187b7c80f31d91689
Stefan Zaufl <stefan.zaufl@hotmail.com>
2016-04-20 15:57:08 +0200

    Added the C1 interpolation

--- UE 3/interpolateC1.m
+++ UE 3/interpolateC1.m
@@ -1,7 +1,51 @@
-function [ output_args ] = interpolateC1( input_args )
+function [ s, bezierPoints ] = interpolateC1( knots, p, firstDerivatives )
 %INTERPOLATEC1 Summary of this function goes here
 %   Detailed explanation goes here
+u = knots(1) : 0.01 : knots(size(knots, 2));
+s = zeros(size(p, 1), size(u, 2));
+bezierPoints = zeros(size(p, 1), 4, size(knots, 2)-1);
+nextBorder = knots(2);
+currentSegment = 1;
 
+bezierPoints = computeBezierPoints(p(:,1), p(:,2), firstDerivatives(:, 1), firstDerivatives(:, 2), knots(2) - knots(1), bezierPoints, currentSegment);
+
+for i = 1 : size(u, 2)
+    while u(i) >= nextBorder && u(i) < knots(size(knots, 2))
+        currentSegment = currentSegment + 1;
+        nextBorder = knots(currentSegment + 1);
+        
+        bezierPoints = computeBezierPoints(p(:,currentSegment), p(:,currentSegment+1), firstDerivatives(:, currentSegment), firstDerivatives(:, currentSegment+1), knots(currentSegment+1) - knots(currentSegment), bezierPoints, currentSegment);
+    end
+    
+    if  u(i) >= knots(size(knots, 2))
+        s(:,i) = p(:, size(p, 2));
+    else
+        dI = knots(currentSegment+1) - knots(currentSegment);
+        t = (u(i) - knots(currentSegment))/dI;
+        s(:, i) = hermite(0, t)*p(:, currentSegment) + dI*hermite(1, t)*firstDerivatives(:, currentSegment) + dI*hermite(2, t)*firstDerivatives(:, currentSegment+1) + hermite(3, t)*p(:, currentSegment+1);
+    end
+end
 
 end
 
+function [h] = hermite(i, t)
+    switch i
+        case 0
+            h = evalBernstein(3, 0, t) + evalBernstein(3, 1, t);
+        case 1
+            h = evalBernstein(3, 1, t)/3;
+        case 2
+            h = evalBernstein(3, 2, t)/-3;
+        case 3
+            h = evalBernstein(3, 2, t) + evalBernstein(3, 3, t);
+        otherwise
+            error(strcat('Invalid hermite parameter i=', num2str(i)))
+    end
+end
+
+function [bezierPoints] = computeBezierPoints(currentPoint, nextPoint, currentDeriv, nextDeriv, deltaI, bezierPoints, segement)
+    bezierPoints(:, 1, segement) = currentPoint;
+    bezierPoints(:, 2, segement) = currentPoint + deltaI/3*currentDeriv;
+    bezierPoints(:, 3, segement) = nextPoint - deltaI/3*nextDeriv;
+    bezierPoints(:, 4, segement) = nextPoint;
+end
--- UE 3/main.m
+++ UE 3/main.m
@@ -16,10 +16,10 @@ y2 = ones(1, size(uChord, 2)).*2;
 y3 = ones(1, size(uLee, 2)).*3;
 
 figure
-plot(uEqui, y1, '-X')
+plot(uEqui, y1, '-Xr')
 hold on
-plot(uChord, y2, '--X')
-plot(uLee, y3, '-.X')
+plot(uChord, y2, '--Xg')
+plot(uLee, y3, '-.Xb')
 hold off
 legend('Equidistant', 'ChordLength', 'Lee')
 axis([0 1 0.5 3.5]);
@@ -44,12 +44,19 @@ mEquiBessel = getDerivative(p, uEqui, 'Bessel');
 mLeeBessel = getDerivative(p, uLee, 'Bessel');
 
 % Interpolate using a C1 spline
-s1ChordFMILL = interpolateC1(uChord, p, mChordFMILL);
-s1EquiFMILL = interpolateC1(uEqui, p, mEquiFMILL);
-s1LeeFMILL = interpolateC1(uLee, p, mLeeFMILL);
-s1ChordBessel = interpolateC1(uChord, p, mChordBessel);
-s1EquiBessel = interpolateC1(uEqui, p, mEquiBessel);
-s1LeeBessel = interpolateC1(uLee, p, mLeeBessel);
+[s1ChordFMILL, s1ChordFMILLBez ] = interpolateC1(uChord, p, mChordFMILL);
+[s1EquiFMILL, s1EquiFMILLBez] = interpolateC1(uEqui, p, mEquiFMILL);
+[s1LeeFMILL, s1LeeFMILLBez] = interpolateC1(uLee, p, mLeeFMILL);
+[s1ChordBessel, s1ChordBesselBez] = interpolateC1(uChord, p, mChordBessel);
+[s1EquiBessel, s1EquiBesselBez] = interpolateC1(uEqui, p, mEquiBessel);
+[s1LeeBessel, s1LeeBesselBez] = interpolateC1(uLee, p, mLeeBessel);
+
+drawSpline(s1ChordFMILL, s1ChordFMILLBez, p, 'C1 Chord FMILL')
+drawSpline(s1EquiFMILL, s1EquiFMILLBez, p, 'C1 Equi FMILL')
+drawSpline(s1LeeFMILL, s1LeeFMILLBez, p, 'C1 Lee FMILL')
+drawSpline(s1ChordBessel, s1ChordBesselBez, p, 'C1 Chord Bessel')
+drawSpline(s1EquiBessel, s1EquiBesselBez, p, 'C1 Equi Bessel')
+drawSpline(s1LeeBessel, s1LeeBesselBez, p, 'C1 Lee Bessel')
 
 % Interpolate using a C2 spline
 s2Chord = interpolateC2(uChord, p);
