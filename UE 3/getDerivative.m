822f893c17a7dcbef03b172296ab990383326d13
HannaHuber <hanna_huber@hotmail.com>
2016-04-20 14:16:00 +0200

    implement derivatives + add test plot to main

--- UE 3/getDerivative.m
+++ UE 3/getDerivative.m
@@ -1,7 +1,49 @@
-function [ m ] = getBesselDeriv( u, method )
-%GETBESSELDERIV Summary of this function goes here
-%   Detailed explanation goes here
+function [ m ] = getDerivative( p, u, method )
+%GETDERIVATIVE For cubic C1 interpolation
 
+switch method
+    case 'FMILL'
+        m = fmillDerivative(p, u);
+    case 'Bessel'
+        m = besselDerivative(p, u);
+    otherwise
+        m = NaN;
+        error('No method specified!'); 
+end
 
 end
 
+function m = besselDerivative(p, u)
+    [dim, k] = size(p);
+    m = zeros(dim, k);
+    
+    % mi
+    for i = 2:k-1
+        ai = (u(i) - u(i-1)) / (u(i+1) - u(i-1));
+        a1 = (1 - ai)/(u(i)-u(i-1));
+        a2 = ai/(u(i+1)-u(i));
+        m(:,i) = a1 * (p(:,i)-p(:,i-1)) + a2 * (p(:,i+1)-p(:,i));
+    end
+    
+    % m0
+    m(:,1) = 2/(u(2)-u(1)) * (p(:,2)-p(:,1)) - m(:,2);
+    
+    % mk
+    m(:,k) = 2/(u(k)-u(k-1)) * (p(:,k)-p(:,k-1)) - m(:,k-1);
+    
+end
+
+function m = fmillDerivative(p, u)
+    [dim, k] = size(p);
+    m = zeros(dim, k);
+    
+    % m0 = mk = 0
+    
+    % mi
+    for i = 2:k-1
+        a1 = 1/ (u(i+1) - u(i-1));
+        a2 = (p(:,i+1) - p(:,i-1)) / norm(p(:,i+1) - p(:,i-1));
+        m(:,i) = a1 * a2;
+    end   
+    
+end
--- UE 3/getKnots.m
+++ UE 3/getKnots.m
@@ -2,6 +2,7 @@ function [ u ] = getKnots( p, method )
 %GETLEEKNOTS Summary of this function goes here
 %   Detailed explanation goes here
 
+u = 0;
 
 end
 
