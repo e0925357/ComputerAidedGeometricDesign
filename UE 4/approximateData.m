1451ab17903bda58abf69af3eeba4c17dd1bdb78
HannaHuber <hanna_huber@hotmail.com>
2016-05-11 11:59:10 +0200

    add UE4 folder: main.m + approximateData.m

---
+++ UE 4/approximateData.m
@@ -0,0 +1,7 @@
+function [ output_args ] = approximateData( input_args )
+%APPROXIMATEDATA Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ UE 4/main.m
@@ -0,0 +1,10 @@
+% CAGD UE4 - Spline approximation
+
+% Get sample points
+fig = figure('Name', 'Spline Approxiamtion: Sample Points');
+plot([0 100], [0 100]);
+[xData, yData] = getpts(fig);
+
+% A: Spline approximation
+
+% B: Parameter correction
