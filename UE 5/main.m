cbde4bfbc6b0a249cf8a59c44c88cad25e6c2203
HannaHuber <hanna_huber@hotmail.com>
2016-06-07 13:19:34 +0200

    add noise to zdata + adjust plots
    +samples: 1x surf, 1x markers
    +zpure vs znoise

--- UE 5/main.m
+++ UE 5/main.m
@@ -1,11 +1,15 @@
-%% CAGD: UE 5 - Task 1
+%% CAGD: UE 5
 close all
 clc
 
+%% Task 1: Surface interpolation
+
 % Data
 [x, y] = meshgrid(1:100,1:100);
-z = x.^2 - y.^2;
-nPoints = 50;
+error = randn(100);
+zPure = x.^2 - y.^2;
+z =  zPure + error;
+nPoints = 10;
 ind = [1 sort(randperm(98,nPoints -2)+1) 100];
 xr = x(1:nPoints, ind);
 yr = y(ind, 1:nPoints);
@@ -39,8 +43,23 @@ colormap(winter);
 
 % Compare with sampled data
 figure(2)
-surf(xr, yr, zr, ones(size(zr)));
+%surf(xr, yr, zr, ones(size(zr)));
+plot3(xr(:), yr(:), zr(:), 'r.', 'MarkerSize', 5);
 hold on
 surf(permute(s(1,:,:), [2 3 1]), permute(s(2,:,:), [2 3 1]), permute(s(3,:,:), [2 3 1]), zeros(size(permute(s(2,:,:), [2 3 1]))));
 colormap(winter);
+
+figure(3)
+surf(xr, yr, zr, ones(size(zr)));
+%plot3(xr(:), yr(:), zr(:), 'r.', 'MarkerSize', 5);
+hold on
+surf(permute(s(1,:,:), [2 3 1]), permute(s(2,:,:), [2 3 1]), permute(s(3,:,:), [2 3 1]), zeros(size(permute(s(2,:,:), [2 3 1]))));
+colormap(winter);
+
+figure(4)
+surf(x, y, z, zeros(size(z)));
+hold on
+surf(x, y, zPure, ones(size(zPure)));
+colormap(winter);
+%% Task 2: Surface approximation
 
